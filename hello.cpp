613afb6b7ae304132f7808703a79e504f334b2ae
jiraya_ <mgautam@me.iitr.ac.in>
2020-07-24 17:58:52 +0530

    hello done

---
+++ hello.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define pu push_back
+#define fi first
+#define se second
+#define mp make_pair
+#define int long long
+#define pii pair<int,int>
+#define mm (s+e)/2
+#define all(x) x.begin(), x.end()
+#define For(i, st, en) for(int i=st; i<en; i++)
+#define tr(x) for(auto it=x.begin(); it!=x.end(); it++)
+#define fast std::ios::sync_with_stdio(false);cin.tie(NULL);
+#define sz 200000
+signed main()
+{
+  
+    cout<<"hello git";
+  
+}
